8739156a93181004a2a58775aaa4d932579d7afc
deyvidandradees <deyvid-asilva@hotmail.com>
2023-08-18 22:04:10 -0300

    + Adição de AssistenteNotificacoes.kt + Adição de AssistentePreferencias.kt * Atualização de codigos de acesso a notificações e preferencias * Update versão

--- src/main/java/com/deyvidandrades/meusdias/servicos/ActionReceiver.kt
+++ src/main/java/com/deyvidandrades/meusdias/servicos/ActionReceiver.kt
@@ -4,6 +4,7 @@ import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.deyvidandrades.meusdias.assistentes.AssistentePreferencias
 import java.util.Calendar
 
 class ActionReceiver : BroadcastReceiver() {
@@ -15,12 +16,11 @@ class ActionReceiver : BroadcastReceiver() {
         notificationManager.cancel(2)
 
         if (!bundle!!.getBoolean("cumpriu", false)) {
-            val sharedPref = context.getSharedPreferences("meus_dias", Context.MODE_PRIVATE)
-
-            with(sharedPref.edit()) {
-                putString("primeiro", Calendar.getInstance().timeInMillis.toString())
-                apply()
-            }
+            AssistentePreferencias.salvarPreferencia(
+                context,
+                AssistentePreferencias.Companion.Chaves.PRIMEIRO,
+                Calendar.getInstance().timeInMillis.toString()
+            )
         }
     }
 }
