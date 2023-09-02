3fefe053109e6ab768e4a58cfa856ea28b067e0a
deyvidandradees <deyvid-asilva@hotmail.com>
2023-09-02 16:20:23 -0300

    + Screenshot agora é salvo em cache - MediaStore não é mais usado + Adição de file provider no manifesto

--- src/main/java/com/deyvidandrades/meusdias/assistentes/AssistenteViewToBitmap.kt
+++ src/main/java/com/deyvidandrades/meusdias/assistentes/AssistenteViewToBitmap.kt
@@ -9,14 +9,41 @@ import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import android.view.View
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 
 class AssistenteViewToBitmap {
 
     companion object {
         fun getViewToBitmapURI(context: Context, printView: View): Uri? {
-            return saveScreenshot(context, printView)
+            return cacheScreenshot(context, printView)
         }
+
+        private fun removerPrintAntigo(context: Context, uri: Uri) {
+            context.contentResolver.delete(
+                uri,
+                "${MediaStore.Images.Media.DISPLAY_NAME} = ?",
+                arrayOf("screenshot_meus_dias.png")
+            )
+        }
+
+        private fun cacheScreenshot(context: Context, printView: View): Uri? {
+
+            val bitmap = getBitmapFromView(printView)
+            val cachePath = File(context.cacheDir, "images")
+
+            cachePath.mkdirs()
+
+            val file = File(cachePath, "recorde_meus_dias.png")
+            val fileOutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream)
+            fileOutputStream.close()
+
+            return FileProvider.getUriForFile(context, "com.deyvidandrades.meusdias.provider", file)
+        }
+
         private fun saveScreenshot(context: Context, printView: View): Uri? {
             // Save the screenshot using MediaStore
             val bitmap = getBitmapFromView(printView)
