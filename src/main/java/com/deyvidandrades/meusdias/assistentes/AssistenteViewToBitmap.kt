dfba0a74200d11c7322ce77c28ff0602c5a8b17c
deyvidandradees <deyvid-asilva@hotmail.com>
2024-04-15 21:12:40 -0300

    - Remoção de funções não utilizadas

--- src/main/java/com/deyvidandrades/meusdias/assistentes/AssistenteViewToBitmap.kt
+++ src/main/java/com/deyvidandrades/meusdias/assistentes/AssistenteViewToBitmap.kt
@@ -1,30 +1,18 @@
 package com.deyvidandrades.meusdias.assistentes
 
-import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.net.Uri
-import android.os.Environment
-import android.provider.MediaStore
 import android.view.View
 import androidx.core.content.FileProvider
 import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
 
 object AssistenteViewToBitmap {
 
     fun getViewToBitmapURI(context: Context, printView: View): Uri? {
         return cacheScreenshot(context, printView)
-    }
-
-    private fun removerPrintAntigo(context: Context, uri: Uri) {
-        context.contentResolver.delete(
-            uri,
-            "${MediaStore.Images.Media.DISPLAY_NAME} = ?",
-            arrayOf("screenshot_meus_dias.png")
-        )
     }
 
     private fun cacheScreenshot(context: Context, printView: View): Uri? {
@@ -40,33 +28,6 @@ object AssistenteViewToBitmap {
         fileOutputStream.close()
 
         return FileProvider.getUriForFile(context, "com.deyvidandrades.meusdias.provider", file)
-    }
-
-    private fun saveScreenshot(context: Context, printView: View): Uri? {
-        // Save the screenshot using MediaStore
-        val bitmap = getBitmapFromView(printView)
-        val displayName = "screenshot_meus_dias.png"
-        val mimeType = "image/png"
-
-        val values = ContentValues()
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, displayName)
-        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
-
-        val contentResolver = context.contentResolver
-        values.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + "/MeusDias")
-
-        // Save the image
-        val imageUri =
-            contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-        try {
-            val outputStream = contentResolver.openOutputStream(imageUri!!)
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream!!)
-            outputStream.close()
-            return imageUri
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return null
     }
 
     private fun getBitmapFromView(view: View): Bitmap {
--- src/main/java/com/deyvidandrades/meusdias/servicos/ActionReceiver.kt
+++ src/main/java/com/deyvidandrades/meusdias/servicos/ActionReceiver.kt
@@ -4,7 +4,6 @@ import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.deyvidandrades.meusdias.R
 import com.deyvidandrades.meusdias.assistentes.NotificacoesUtil
 import com.deyvidandrades.meusdias.assistentes.Persistencia
