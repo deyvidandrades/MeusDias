b1c72cd9efb8789878351342ade473416cae97e6
deyvidandrades <deyvid-asilva@hotmail.com>
2024-07-13 18:41:49 -0300

    * Gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,10 @@ android {
 
     defaultConfig {
         applicationId = "com.deyvidandrades.meusdias"
-        minSdk = 29
+        minSdk = 31
         targetSdk = 34
-        versionCode = 29
-        versionName = "1.3.14"
+        versionCode = 30
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -27,7 +27,7 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility =  JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
@@ -39,9 +39,9 @@ android {
 dependencies {
 
     //Core libs
-    implementation("androidx.core:core-ktx:1.13.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     //Android widgets
     implementation("androidx.core:core-splashscreen:1.0.1")
@@ -51,10 +51,10 @@ dependencies {
 
     //Outras bibliotecas
     implementation("nl.dionsegijn:konfetti-xml:2.0.4")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.11.0")
 
     //Test libraries
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
