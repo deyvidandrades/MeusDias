ff757b3848f1b0a4a4c366d5919ca9ce865a2f05
deyvidandrades <deyvid-asilva@hotmail.com>
2025-07-28 07:17:19 -0300

    * Gradle update * Version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,21 +5,21 @@ plugins {
 
 android {
     namespace = "com.deyvidandrades.meusdias"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.deyvidandrades.meusdias"
-        minSdk = 31
-        targetSdk = 34
-        versionCode = 31
-        versionName = "1.4.1"
+        minSdk = 35
+        targetSdk = 36
+        versionCode = 32
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -27,31 +27,31 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
-    buildToolsVersion = "34.0.0"
+    buildToolsVersion = "35.0.0"
 }
 
 dependencies {
 
     //Core libs
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
 
     //Android widgets
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("com.google.android.play:review:2.0.1")
-    implementation("com.google.android.play:review-ktx:2.0.1")
+    implementation("com.google.android.play:review:2.0.2")
+    implementation("com.google.android.play:review-ktx:2.0.2")
 
     //Outras bibliotecas
-    implementation("nl.dionsegijn:konfetti-xml:2.0.4")
-    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("nl.dionsegijn:konfetti-xml:2.0.5")
+    implementation("com.google.code.gson:gson:2.13.1")
 
     //Test libraries
     testImplementation("junit:junit:4.13.2")
