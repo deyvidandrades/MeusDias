2f3fd9f85ca5f3f6591c470609330d3eae9be9d3
deyvidandrades <deyvid-asilva@hotmail.com>
2025-08-04 09:39:31 -0300

    * gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,6 @@ dependencies {
 
     //Test libraries
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
