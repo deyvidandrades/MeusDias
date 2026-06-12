7db72117c630c56d9d5999aa0b7a09696c7933c3
deyvidandrades <deyvid-asilva@hotmail.com>
2026-06-12 08:11:17 -0300

    * Gradle packages update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application") version "9.2.1"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0"
     kotlin("plugin.serialization") version "1.9.10"
 }
 
 android {
     namespace = "com.deyvidandrades.meusdias"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.deyvidandrades.meusdias"
@@ -14,6 +14,7 @@ android {
         targetSdk = 36
         versionCode = 33
         versionName = "2.4.2"
+        targetSdk = 37
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -37,23 +38,23 @@ android {
 }
 
 dependencies {
-//Core libs
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
+    //Core libs
+    implementation("androidx.core:core-ktx:1.19.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
 
     //Compose
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation(platform("androidx.compose:compose-bom:2025.07.00"))
-    implementation("androidx.compose.ui:ui:1.8.3")
-    implementation("androidx.compose.ui:ui-graphics:1.8.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.3")
-    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("androidx.activity:activity-compose:1.13.0")
+    implementation(platform("androidx.compose:compose-bom:2026.05.01"))
+    implementation("androidx.compose.ui:ui:1.11.2")
+    implementation("androidx.compose.ui:ui-graphics:1.11.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.11.2")
+    implementation("androidx.compose.material3:material3:1.4.0")
     implementation("androidx.compose.material:material-icons-extended")
 
     //Android components
-    implementation("androidx.work:work-runtime-ktx:2.10.3")
-    implementation("androidx.datastore:datastore-preferences:1.1.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("androidx.work:work-runtime-ktx:2.11.2")
+    implementation("androidx.datastore:datastore-preferences:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
 
     //Other libs
     implementation("nl.dionsegijn:konfetti-compose:2.0.5")
@@ -63,8 +64,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.07.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.8.3")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.3")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2026.05.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.11.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.11.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.11.2")
 }
